b0d4109c2039e3a24e081d977b55b7aa8a41848d
zac <yoking@outlook.com>
2024-03-16 17:24:38 +0800

    test

--- test/sse.spec.ts
+++ test/sse.spec.ts
@@ -4,32 +4,31 @@ const decoder = new SSEDecoder();
 
 describe('SSEDecoder', () => {  
   test('should decode data-only messages', () => {
-    const data = '{"username": "bobby", "time": "02:33:48"}';
-    const line = `data: ${data}`;
+    const raw = '{"username": "bobby", "time": "02:33:48"}';
+    const line = `data: ${raw}`;
     const buf = Buffer.from(`${line}\n\n`, 'utf-8');
-    const event = decoder.decode(buf);
-    expect(event).toEqual({
+    const data = decoder.decode(buf)[0];
+    expect(data).toEqual({
       event: null,
-      data,
+      data: raw,
       raw: [line]
     });
   });
 
   test('should decode named events', () => {
-    const data = '{"username": "bobby", "time": "02:33:48"}';
-    const lines = `:HTTP 1.1\nid: 1\nevent: result\ndata: ${data}`;
+    const raw = '{"username": "bobby", "time": "02:33:48"}';
+    const lines = `:HTTP 1.1\nid: 1\nevent: result\ndata: ${raw}`;
     const buf = Buffer.from(`${lines}\n\n`, 'utf-8');
-    const event = decoder.decode(buf);
-    expect(event).toEqual({
+    const data = decoder.decode(buf)[0];
+    expect(data).toEqual({
       event: 'result',
-      data,
+      data: raw,
       raw: [
         ':HTTP 1.1',
         'id: 1',
         'event: result',
-        `data: ${data}`
+        `data: ${raw}`
       ]
     });
   });
-
 });
